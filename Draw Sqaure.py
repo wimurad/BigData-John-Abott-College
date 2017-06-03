eca8c4339799dcc0c96c5c21be674c1b3452db33
wimurad <wahidul.murad@gmail.com>
2017-06-03 15:54:25 -0400

    Add files via upload

---
+++ Draw Sqaure.py
@@ -0,0 +1,19 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor ("blue")
+
+    abcd = turtle. Turtle()
+    abcd.forward(200)
+    abcd.right(90)
+    abcd.forward(200)
+    abcd.right(90)
+    abcd.forward(200)
+    abcd.right(90)
+    abcd.forward(200)
+    abcd.right(90)
+    window.exitonclick()
+draw_square()
+    
+    
