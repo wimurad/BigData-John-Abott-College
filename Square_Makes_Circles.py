5b03f84a429cee97858821ce758e0f319dd1ea82
wimurad <wahidul.murad@gmail.com>
2017-06-10 14:09:36 -0400

    Add files via upload

---
+++ Square_Makes_Circles.py
@@ -0,0 +1,21 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor ("blue")
+    
+    a = turtle. Turtle()
+    turtle.speed (0.1)
+    a.color ("yellow")
+    for circlecount in range (0,36):
+        a.left (10)
+        for count in range (0,4):
+    
+            a.forward(100)
+            a.right(90)
+              
+    window.exitonclick()
+
+draw_square()
+    
+    
